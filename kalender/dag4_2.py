2eacf29a41645f67da98e987f9cf294070cb09a6
jakob1604 <gm.jbsolli@gmail.com>
2023-12-07 21:39:41 +0100

    part222

--- kalender/dag4_2.py
+++ kalender/dag4_2.py
@@ -1,16 +1,23 @@
-inp = open("dag4.txt","r")
+#inp = open("dag4.txt","r")
+inp = open("dag4_test.txt","r")
 text= inp.read()
 
 text = text.splitlines()
 total = 0
+num = 0
+cache = [1,1,1,1,1,1,1,1,1,1]
+
 for i in range(len(text)):
+    num += 1
     card = text[i].split(":")[1]
     card = card.split("|")
 
     win = []
     right = 0
+    
+    print("Card: ", num)
     for j in range(len(card)):
-
+        
         if (j % 2) == 0:
             case = True
         
@@ -24,7 +31,7 @@ for i in range(len(text)):
         while("" in card2): 
             card2.remove("")        
         
-        print(card2)
+        #print(card2)
 
         if case == True:
             win = card2
@@ -34,17 +41,25 @@ for i in range(len(text)):
             for k in range(len(card2)):
                 if card2[k] in win:
                     right += 1
-    print(right)
-
-    if right > 1:       
-        score = 2**(right - 1)
-    elif right == 1:
-        score = 1
-    elif right == 0:
-        score = 0
-
-
-    print(score)
-    total += score
-
+            
+            if right > 9:
+                right = 9
+            for b in range(0,cache[0]):
+                #print("test")
+                for o in range(0,right):
+                #if cache[0] > 1:
+                #    cache[o+1] += cache[0]
+                #else:
+                    cache[o+1] += 1
+            print("Antall rette denne runden: ",right)
+            print("Ny cache", cache)
+        
+            total += cache[0]
+            print("Total så langt: ", total)
+        
+            del cache[0]
+            cache.append(1)
+            print("Reset cache", cache)
+    print("\n")
+    
 print(total)
