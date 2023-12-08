5ac1f553cbdef45c4449ae18847abf6e5d9147bb
Jakob <gm.jbsolli@gmail.com>
2023-12-08 01:04:01 +0100

    start dag 7

---
+++ kalender/dag7.py
@@ -0,0 +1,13 @@
+inp = open("./kalender/dag7_test.txt","r")
+text= inp.read()
+text = text.splitlines()
+#print(text)
+
+for i in range(len(text)):
+    hand = text[i].split(" ")
+    print(hand)
+
+    def check(x):
+        for b in range(hand):
+            
+    for
